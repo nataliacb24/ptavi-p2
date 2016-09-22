54aff6e2ab9e40381a24af2f7cfe658ccc4ad968
Natalia Carrillo Blanco <ncarrill@alpha24.aulas.gsyc.urjc.es>
2016-09-22 11:42:29 +0200

    Ejercicio 6 sin terminar

--- calcplus.py
+++ calcplus.py
@@ -4,30 +4,47 @@
 import calcoohija
 import sys
 
-fich = open('/tmp/git/fichero', 'r')
+fich = open('/home/alumnos/ncarrill/Escritorio/ptvi/ptavi-p2/fichero', 'r')
 lineas = fich.readlines()
-    
 
-for linea in lineas:
+
+if __name__ == "_main_":
 
     calculadora = calcoohija.CalculadoraHija()
-    
-    elems = linea[:-1].split(',')
-    
-    for elem in elems: 
-    
-        elem = 
         
+    for linea in lineas:
+        
+        elems = linea[:-1].split(',')
+        
+        op1 = int(elems[1])
+        op2 = int(elems[2])
+
         if elems[0] == 'suma':
-            resultado = calculadora.suma(int(elems[1]), int(elems[2]))
+            resultado1 = calculadora.suma(op1, op2)
        
         elif elems[0] == 'resta':
-            resultado = calculadora.resta(int(elems[1]),int(elems[2]))
+            resultado2 = calculadora.resta(op1, op2)
    
         elif elems[0] == 'divide':
-            resultado = calculadora.div(int(elems[1]),int(elems[2]))
-        
-        elif elems[0] == 'multiplica':
-            resultado = calculadora.mult(int(elems[1]),int(elems[2]))
+            resultado3 = calculadora.div(op1, op2)
 
-    print(resultado)
+        elif elems[0] == 'multiplica':
+            resultado4 = calculadora.mult(op1, op2)
+        
+        
+        for elem in elems[3:]:
+        
+            if elems[0] == 'suma':
+                resultado = calculadora.suma(resultado1, int(elem))
+            
+            elif elems[0] == 'resta':
+                resultado = calculadora.resta(resultado2, int(elem))
+            
+            elif elems[0] == 'resta':
+                resultado = calculadora.div(resutlado3, int(elem))
+            
+            elif elems[0] == 'multiplica':
+                resultado = calculadora.div(resultado4, int(elem))
+                
+            print(resultado)
+            
