b20cdd8791699eef1a384a22c633015b8591a34c
Natalia Carrillo Blanco <ncarrill@gamma26.aulas.gsyc.urjc.es>
2016-09-26 10:37:13 +0200

    Calcoo modificado sin espacios

--- calcoo.py
+++ calcoo.py
@@ -7,10 +7,10 @@ class Calculadora():
 
     def suma(self, op1, op2):
         return op1 + op2
-    
+
     def resta(self, op1, op2):
         return op1 - op2
-        
+
 if __name__ == "__main__":
 
     calculadora = Calculadora()
@@ -18,15 +18,15 @@ if __name__ == "__main__":
     try:
       operando1 = int(sys.argv[1])
       operando2 = int(sys.argv[3])
-      
+
     except ValueError:
         sys.exit("Error: Parametros no numericos")
-    
+
     if sys.argv[2] == "suma":
         resultado = calculadora.suma(operando1, operando2)
     elif sys.argv[2] == "resta":
         resultado = calculadora.resta(operando1, operando2)
     else:
         sys.exit("Solo se puede sumar o restar")
-        
+
     print(resultado)
