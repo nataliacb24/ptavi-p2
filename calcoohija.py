101cc9f06122babbe8953fc7d6db1b09206585cf
Natalia Carrillo Blanco <ncarrill@gamma26.aulas.gsyc.urjc.es>
2016-09-26 10:37:23 +0200

    Calcoohija modificado sin espacios

--- calcoohija.py
+++ calcoohija.py
@@ -2,21 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import calcoo
-import sys 
+import sys
 
 
 class CalculadoraHija(calcoo.Calculadora):
 
     def mult(self, op1, op2):
         return op1 * op2
-    
+
     def div(self, op1, op2):
         try:
-            return op1 / op2 
+            return op1 / op2
         except ZeroDivisionError:
             sys.exit("Division by zero is not allowed")
-    
-    
+
 if __name__ == "__main__":
 
     calculadora = CalculadoraHija()
@@ -24,11 +23,10 @@ if __name__ == "__main__":
     try:
       operando1 = int(sys.argv[1])
       operando2 = int(sys.argv[3])
-     
+
     except VauleError:
       sys.exit("Parametros no numericos")
-      
-      
+
     if sys.argv[2] == "suma":
         resultado = calculadora.suma(operando1, operando2)
     elif sys.argv[2] == "resta":
@@ -39,5 +37,5 @@ if __name__ == "__main__":
         resultado = calculadora.mult(operando1, operando2)
     else:
         sys.exit("Operacion no valida")
-        
+
     print(resultado)
