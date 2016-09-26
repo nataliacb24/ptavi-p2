4ab13c014284d3f1a00b60142857e55416169f09
Natalia Carrillo Blanco <ncarrill@gamma26.aulas.gsyc.urjc.es>
2016-09-26 10:37:49 +0200

    Calcplusplus modificado sin espacios

--- calcplusplus.py
+++ calcplusplus.py
@@ -9,7 +9,7 @@ calculadora = calcoohija.CalculadoraHija()
 
 with open(sys.argv[1], newline = '') as fich:
     lineas = csv.reader(fich)
-    for linea in lineas:           
+    for linea in lineas:         
 
         op1 = int(linea[1])
         op2 = int(linea[2])
@@ -25,19 +25,19 @@ with open(sys.argv[1], newline = '') as fich:
 
         elif linea[0] == 'multiplica':
             resultado = calculadora.mult(op1, op2)
-                    
+          
         for elem in linea[3:]:
-               
+
             if linea[0] == 'suma':
                 resultado = calculadora.suma(resultado,int(elem))
-                                 
+
             elif linea[0] == 'resta':
                 resultado = calculadora.resta(resultado, int(elem))
-                    
+
             elif linea[0] == 'divide':
                 resultado = calculadora.div(resultado, int(elem))
-                    
+
             elif linea[0] == 'multiplica':
                 resultado = calculadora.mult(resultado, int(elem))
-            
+
         print(resultado)
