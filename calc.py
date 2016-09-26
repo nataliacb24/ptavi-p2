f1952a29661d23da0dec1c06615cf8bf5eafba8d
Natalia Carrillo Blanco <ncarrill@gamma26.aulas.gsyc.urjc.es>
2016-09-26 10:37:04 +0200

    Calc modificado sin espacios

--- calc.py
+++ calc.py
@@ -3,11 +3,9 @@
 
 import sys
 
-
 def plus(op1, op2):
     """ Function to sum the operands """
     return op1 + op2
-
 
 def minus(op1, op2):
     """ Function to substract the operands """
@@ -16,15 +14,14 @@ def minus(op1, op2):
 def multiplicar(op1,op2):
     """ Funcion para multiplicar dos operandos """
     return op1 * op2
-    
+
 def dividir(op1,op2):
     """Funcion para dividir dos operandos """
     try:
         return op1/op2    
-        
+
     except ZeroDivisionError:
        sys.exit("Error: El divisor no puede ser cero")
-    
 
 if __name__ == "__main__":
 
@@ -33,7 +30,7 @@ if __name__ == "__main__":
         operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-    
+
     if sys.argv[2] == "suma":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
