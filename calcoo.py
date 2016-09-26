3c6f34fa4651cca082bc3497587b7bbf564cf856
Natalia Carrillo Blanco <ncarrill@gamma27.aulas.gsyc.urjc.es>
2016-09-26 15:19:12 +0200

    Comprobacion calcoo

--- calcoo.py
+++ calcoo.py
@@ -4,26 +4,28 @@
 import sys
 
 class Calculadora():
-    def suma(self, op1, op2):
-        return op1 + op2
+	def suma(self, op1, op2):
+		return op1 + op2
 
-    def resta(self, op1, op2):
-        return op1 - op2
+	def resta(self, op1, op2):
+		return op1 - op2
 
 if __name__ == "__main__":
 
-    calculadora = Calculadora()
-    try:
-      operando1 = int(sys.argv[1])
-      operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Parametros no numericos")
+	calculadora = Calculadora()
 
-    if sys.argv[2] == "suma":
-        resultado = calculadora.suma(operando1, operando2)
-    elif sys.argv[2] == "resta":
-        resultado = calculadora.resta(operando1, operando2)
-    else:
-        sys.exit("Solo se puede sumar o restar")
+	try:
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])
+		
+	except ValueError:
+		sys.exit("Error: Parametros no numericos")
 
-    print(resultado)
+	if sys.argv[2] == "suma":
+		resultado = calculadora.suma(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		resultado = calculadora.resta(operando1, operando2)
+	else:
+		sys.exit("Solo se puede sumar o restar")
+
+	print(resultado)
